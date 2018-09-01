0a678867094693570d5d8b3be91d1323278f3c43
gsalaun1 <salaunga@yahoo.fr>
2018-09-01 16:34:53 +0200

    Spring Boot Java OK

--- spring-boot-java/src/main/java/gsalaun1/extreme_startup_spring_boot_java/controller/ExtremeStartupController.java
+++ spring-boot-java/src/main/java/gsalaun1/extreme_startup_spring_boot_java/controller/ExtremeStartupController.java
@@ -1,8 +1,35 @@
 package gsalaun1.extreme_startup_spring_boot_java.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * <p>ExtremeStartupController</p>
  */
+@RestController
 public class ExtremeStartupController {
+
+    @GetMapping(name = "/")
+    public String get(HttpServletRequest request) {
+        System.out.println("A request has arrived");
+        return answer(request.getParameter("q"));
+    }
+
+    String answer(String parameter) {
+        if (Objects.isNull(parameter)) {
+            return "team name";
+        }
+        Matcher additionMatcher = Pattern.compile(".*what is the sum of (\\d+) and (\\d+)").matcher(parameter);
+        if (additionMatcher.matches()) {
+            return String.valueOf(Integer.parseInt(additionMatcher.group(1))
+                    + Integer.parseInt(additionMatcher.group(2)));
+        }
+        return "team name";
+    }
 
 }
