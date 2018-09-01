6623215b7349b8439528207a22835513bcfa4875
gsalaun1 <salaunga@yahoo.fr>
2018-09-01 16:22:37 +0200

    Init Spring Boot Java

---
+++ spring-boot-java/src/main/java/gsalaun1/extreme_startup_spring_boot_java/ExtremeStartupSpringBootJavaApplication.java
@@ -0,0 +1,12 @@
+package gsalaun1.extreme_startup_spring_boot_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtremeStartupSpringBootJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtremeStartupSpringBootJavaApplication.class, args);
+    }
+}
---
+++ spring-boot-java/src/test/java/gsalaun1/extreme_startup_spring_boot_java/ExtremeStartupSpringBootJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package gsalaun1.extreme_startup_spring_boot_java;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExtremeStartupSpringBootJavaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
